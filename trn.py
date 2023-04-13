be2a8046c02419790146a1f867ca1c9cf4a98fae
Kataplan <dcatalanmarchese@gmail.com>
2023-04-13 17:55:20 -0400

    update trn.py

--- trn.py
+++ trn.py
@@ -9,33 +9,87 @@ def save_w_mse():
     ...
     return
 
-#gets Index for n-th miniBatch
-def get_Idx_n_Batch(n,x,N):
-    ...    
-    return(Idx)
-
 #miniBatch-SGDM's Training 
-def trn_minibatch(x,y,param):    
-    W,V   = iniWs()
-    ....
-    return(...)
+def trn_minibatch(x,y,W,param):
+    M = int(param[8])
+    N = x.shape[0]
+    nBatch = N // M
+    print(nBatch)
+    act_f = int(param[6])
+    
+    # Training loop
+    for n in range(1, nBatch):
+        Idx = get_Idx_n_Batch(n, M)
+        print(Idx)
+        xe = x[:,:Idx[len(Idx)]]
+        ye = y[:,Idx]
+        Act = ut.forward(xe, W, act_f)
+        print(Act)
+        gW, cost = ut.gradW(Act, ye, W, param)
+        W, V = ut.updWV_sgdm(W, gW, param)
+    
+    return cost, W
 
 #SNN's Training 
-def train(x,y,param):    
-    W,V   = iniWs()
-    ....
-    return(W,Costo)
+def train(x,y,param):
+
+    n_input_nodes = x.shape[1]
+    n_output_nodes = y[0].shape[0]
+    n_hidden_1 = int(param[4])
+    n_hidden_2 = int(param[5])
+    iter = int(param[11])
+    # SetUp pesos 
+    L = 3
+    
+    nodes = [n_input_nodes]
+    
+    if n_hidden_1 > 0:
+        nodes.append(n_hidden_1)
+    else:
+        L = -1
+    if n_hidden_2 > 0:
+        nodes.append(n_hidden_2)
+    else:
+        L = -1
+    nodes.append(n_output_nodes)
+
+    W = ut.iniWs(L, nodes)
+    MSE = []
+
+    for i in range(iter):
+        x,y = sort_data_ramdom(x,y)
+        cost, W = trn_minibatch(x,y,W, param) 
+
+    return(W,cost)
+
+# Function to get the indices of the n-th batch
+def get_Idx_n_Batch(n, M):
+    start = (n-1)*M
+    end = n*M
+    return np.arange(start, end)
+
+def sort_data_ramdom(X,Y):
+
+    XY = np.concatenate((X,Y), axis=1)
+    np.random.shuffle(XY)
+    X_new, Y_new = np.split(XY, [X.shape[1]], axis=1)
+    return X_new,Y_new
 
 # Load data to train the SNN
-def load_data_trn():
-    ....
-    return()
+def load_data_trn(param):
+    n = int(param[0])
+    data = np.genfromtxt('dtrn.csv', delimiter=',')
+    x = data[:,:-n]
+    y = data[:,-n:]
+    return(x,y)
     
-   
+def save_w_cost():
+    
+    return  
 # Beginning ...
 def main():
     param       = ut.load_cnf()            
-    xe,ye       = load_data_trn()   
+    xe,ye       = load_data_trn(param)   
     W,Cost      = train(xe,ye,param)             
     save_w_cost(W,Cost)
        
